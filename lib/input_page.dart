aafa693bd8faceccada3197baa89110227073aca
Adhiraj772 <adhirajsaxena123@gmail.com>
2021-12-11 01:25:26 +0530

    Earlier we were using the same code again and again to create our container, so what we did is we have used the extract widget property from flutter outline, this made a custom widget for our container.We just need to call the widget to build the container. Then in the widget class we created a property of color that will be required and has to be passed while calling the widget.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,54 +17,24 @@ class _InputPageState extends State<InputPage> {
               child:Row(
                 children: <Widget>[
                   Expanded(
-                      child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                      child: ReusableCard(colour: Color(0xFF1D1E33),),
                   ),
                   Expanded(
-                      child: Container(
-                        margin: EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF1D1E33),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
+                      child:ReusableCard(colour: Color(0xFF1D1E33),),
                   ),
                 ],
               ) ,
           ),
           Expanded(
-              child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
+              child: ReusableCard(colour: Color(0xFF1D1E33),),
           ),
           Expanded(child: Row(
             children: <Widget>[
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
+                child: ReusableCard(colour: Color(0xFF1D1E33),),
               ),
               Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
               ),
             ],
           ),
@@ -73,5 +43,22 @@ class _InputPageState extends State<InputPage> {
       ),
 
     );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+    color: colour,
+    borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  );
   }
 }
